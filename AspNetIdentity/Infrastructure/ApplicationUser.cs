ddbc297550fdabb2a6d6ee27f8431c17e1b02df0
IngAnibalGuerra <inganibalguerra@gmail.com>
2016-10-09 15:24:45 -0500

    identity confirm email

---
+++ AspNetIdentity/Infrastructure/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspNetIdentity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+
+    }
+}
---
+++ AspNetIdentity/Services/EmailService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+//cOMENT//Ario nuevo
+namespace AspNetIdentity.Services
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            //await configSendGridasync(message);
+            await configSendSmtp(message);
+        }
+        private async Task configSendSmtp(IdentityMessage message)
+        {
+            using (System.Net.Mail.MailMessage MailSetup = new System.Net.Mail.MailMessage())
+            {
+                NetworkCredential loginInfo = new NetworkCredential("aqui tu correo", "aqui tu contraseña");
+                MailSetup.Subject = message.Subject;
+                MailSetup.To.Add(message.Destination);
+                MailSetup.From = new System.Net.Mail.MailAddress("tramites@valledupar.gov.co", "Avanzar es Posible!!!");
+                MailSetup.Body = message.Body;
+                using (System.Net.Mail.SmtpClient SMTP = new System.Net.Mail.SmtpClient("smtp.live.com"))
+                {
+                    SMTP.Port = 587;
+                    SMTP.EnableSsl = true;
+                    SMTP.Credentials = loginInfo;
+                    await SMTP.SendMailAsync(MailSetup);
+                }
+            }
+
+
+        }
+        
+    }
+}
